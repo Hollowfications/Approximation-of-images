68b6ccff93ebfe6040957877d10ddd740dee3401
Recreatorms <43077503+Recreatorms@users.noreply.github.com>
2020-06-29 18:54:13 +0300

    Обновление документации
    дорогие друзья, ВСЁ

--- source/state.js
+++ source/state.js
@@ -1,14 +1,15 @@
 /**
  * @file Файл, содержащий вспомогательный класс {State} State.
  */
-/**
- * @class State - текущее состояние
- * @param {Canvas} target - следующее состояние
- * @param {Canvas} canvas - текущее состояние - в виде холста
- * @param distance - расстояние между пикселями прошлого и текущего изображений
- */
-export default class State {
 
+export default class State {
+    /**
+     * Текущее состояние
+     * @class State
+     * @param {Canvas} target - следующее состояние
+     * @param {Canvas} canvas - текущее состояние - в виде холста
+     * @param distance - расстояние между пикселями прошлого и текущего изображений
+     */
     constructor(target, canvas, distance = Infinity) {
         this.target = target;
         this.canvas = canvas;
